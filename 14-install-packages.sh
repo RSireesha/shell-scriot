fa9feff1cf002c7e3763374395badd7af49b7f5c
RSireesha <ramireddysireesha20@gmail.com>
2024-04-18 19:33:19 +0530

    installation

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\E[31M"
+G="\E[32M"
+N="\E[0M"
+
+VALIDATE(){
+ if [ $1 -ne 0 ]
+ then
+    echo "$2... $R FAILURE $N"
+    exit 1
+else 
+    echo "$2... $G SUCCESS $N"
+fi
+}
+
 if [ $USERID -ne 0 ]
 then 
     echo "Please run this script with root access."
@@ -9,8 +26,16 @@ else
    echo "You are a super user."
 fi
 
+
 for i in $@
 do 
   echo "package to install: $i"
-
+  dnf list installed $i &>>$LOGFILE
+  if [ $? -eq 0 ] 
+  then 
+      echo -e "$i already installed...$Y SKIPPING $N"
+  else
+      dnf install $i -y&>>$LOGFILE
+      VALIDATE $? "Installation of $i"
+  fi
 done
