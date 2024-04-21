bf02d9e76abd64df0acbdba1c4b55e443b208f18
RSireesha <ramireddysireesha20@gmail.com>
2024-04-21 12:28:12 +0530

    packages installation

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -4,17 +4,17 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\E[31M"
-G="\E[32M"
-N="\E[0M"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 VALIDATE(){
  if [ $1 -ne 0 ]
  then
-    echo "$2... $R FAILURE $N"
+    echo -e "$2...$R FAILURE $N"
     exit 1
 else 
-    echo "$2... $G SUCCESS $N"
+    echo -e "$2...$G SUCCESS $N"
 fi
 }
 
