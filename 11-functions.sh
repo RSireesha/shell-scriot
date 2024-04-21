45135040b1776f77566761cc4f11bbfd7bf707b3
RSireesha <ramireddysireesha20@gmail.com>
2024-04-21 11:44:36 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -6,26 +6,25 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE(){
- if [ $1 -ne 0 ]
- then
-    echo "$2...FAILURE"
-    exit 1
-else 
-    echo "$2...SUCCESS"
-fi
+   if [ $1 -ne 0 ]
+   then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi
 }
 
 if [ $USERID -ne 0 ]
-then 
+then
     echo "Please run this script with root access."
-    exit 1 # manually exist if error comes.
+    exit 1 # manually exit if error comes.
 else
-   echo "You are a super user."
+    echo "You are super user."
 fi
 
 dnf install mysql -y &>>$LOGFILE
-VALIDATE $? "Installing MySql"
+VALIDATE $? "Installing MySQL"
 
-dnf insatll git -y  &>>$LOGFILE
+dnf install git -y &>>$LOGFILE
 VALIDATE $? "Installing Git"
-
