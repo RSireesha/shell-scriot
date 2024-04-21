c267bc022d9f9c71af70c4c956753bee6d44f177
RSireesha <ramireddysireesha20@gmail.com>
2024-04-21 10:43:25 +0530

    insatllation

--- 10-installation.sh
+++ 10-installation.sh
@@ -15,13 +15,25 @@
        echo "You are a super user"
     fi
 
-   dnf install mysqll -y # -y is mandatory if we dont mention shell script will keep on waiting
+   dnf install mysql -y # -y is mandatory if we dont mention shell script will keep on waiting
    
    if [$? -ne o]
    then 
        echo "Installation of mysql...FAILURE"
        exit 1
+   else
+       echo "Installation of mysql...SUCCESS"
    fi
    
    dnf install git -y
+
+   if [$? -ne o]
+   then 
+       echo "Installation of git...FAILURE"
+       exit 1
+   else
+       echo "Installation of git...SUCCESS"
+   fi
+
+
     echo "is script proceeding"
